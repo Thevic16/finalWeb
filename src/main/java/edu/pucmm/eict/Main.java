b5facfd366b172991c2d88d25997083c2057f171
Thevic16 <20171408@ce.pucmm.edu.do>
2021-04-02 14:30:37 -0400

    adding statticFiles javalin

--- src/main/java/edu/pucmm/eict/Main.java
+++ src/main/java/edu/pucmm/eict/Main.java
@@ -1,7 +1,14 @@
 package edu.pucmm.eict;
 
+import io.javalin.Javalin;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+
+        //Creando la instancia del servidor.
+        Javalin app = Javalin.create(config ->{
+            config.addStaticFiles("/publico"); //desde la carpeta de resources
+        });
     }
 }
